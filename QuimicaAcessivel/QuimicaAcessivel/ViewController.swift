ee0722ade6685594e40955e73e313c309be46667
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2020-01-16 01:22:25 -0300

    initial commit

---
+++ QuimicaAcessivel/QuimicaAcessivel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuimicaAcessivel
+//
+//  Created by Guilherme Paciulli on 16/01/20.
+//  Copyright © 2020 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}

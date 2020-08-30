bb86769ad0de3bb357cd01e3211753ce46a0e1e2
DevilInside <gpaciulli100@gmail.com>
2020-08-30 11:09:19 -0300

    fixes in tutorial view

---
+++ QuimicaAcessivel/QuimicaAcessivel/ViewController/TutorialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialViewController.swift
+//  QuimicaAcessivel
+//
+//  Created by Guilherme Paciulli on 30/08/20.
+//  Copyright © 2020 Guilherme Paciulli. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+    }
+
+}
